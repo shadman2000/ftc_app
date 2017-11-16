77658a806329077416f5b0c4a2f5a70da0f28d99
Shadman Chowdhury <shadman.c19@bronxsoftware.org>
2017-11-16 20:32:30 +0000

    TestBotOp.java created for 8201 with Hardwarek9Bot

---
+++ Team8201/src/main/java/org/firstinspires/ftc/team8201/HardwareK9bot.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareK9bot
+{
+    /* Public OpMode members. */
+    public DcMotor  leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+    public Servo    arm         = null;
+    public Servo    claw        = null;
+
+    public final static double ARM_HOME = 0.2;
+    public final static double CLAW_HOME = 0.2;
+    public final static double ARM_MIN_RANGE  = 0.20;
+    public final static double ARM_MAX_RANGE  = 0.90;
+    public final static double CLAW_MIN_RANGE  = 0.20;
+    public final static double CLAW_MAX_RANGE  = 0.7;
+
+    /* Local OpMode members. */
+    HardwareMap hwMap  = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareK9bot() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive = hwMap.get(DcMotor.class, "right_drive");
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        arm  = hwMap.get(Servo.class, "arm");
+        claw = hwMap.get(Servo.class, "claw");
+        arm.setPosition(ARM_HOME);
+        claw.setPosition(CLAW_HOME);
+    }
+}
