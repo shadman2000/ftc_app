d2971df234d22131092b8dbaac120da3d2c25aad
Student <student@3-34.78x660.nycboe.org>
2017-11-22 11:23:58 -0500

    clean

---
+++ Team8201/src/main/java/org/firstinspires/ftc/team8201/HardwareK9botTest.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareK9bot
+{
+    /* Public OpMode members. */
+    public dcMotor leftWheel;
+    public dcMotor rightWheel;
+    
+    /* Local OpMode members. */
+    HardwareMap hwMap;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareK9bot() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap; //Get this line clear****
+
+        // Define and Initialize Motors
+        leftWheel  = hwMap.get(DcMotor.class, "leftWheel");
+        rightWheel = hwMap.get(DcMotor.class, "rightWheel");
+
+        // Set all motors to zero power
+        leftWheel.setPower(0);
+        rightWheel.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+}
---
+++ Team8201/src/main/java/org/firstinspires/ftc/team8201/testBotBasicOp.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.team8201;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "Test TeleOp", group = "Testing")
+public class testBotBasicOp extends LinearOpMode {
+    HardwareK9bot robot = new HardwareK9bot();
+    
+    @Override
+    public void runOpMode() {
+        
+        //Declaring the variables
+        
+        //motor left
+        double leftMotorPower = 0.0;
+        
+        //motor right
+        double RightMotorPower =0.0;
+        
+        //Initializing the hardware
+        robot.init(hardwareMap);
+        
+        //Testing messages
+        telemetry.addData("Say ","I am ready 8201 testBot");
+        telemetry.update();
+        
+        //Wait for play
+        waitForStart();
+        
+        //Running everything in the while loop
+        while(opModeIsActive()) {
+            
+            //right_triggr
+            if(gamepad1.right_trigger > 0){
+                
+                //Defining te the powers of both mototrs according the right_trigger positions
+                rightMotorPower = gamepad1.right_trigger;
+                leftMotorPower = gamepad1.right_trigger;
+                
+                //Setting the range of the motor power
+                rightMotorPower = Range.clip(rightMotorPower, 0.0, 1.0);
+                leftMotorPower = Range.clip(leftMotorPower, 0.0, 1.0);
+                
+                //Turning while trigger is pressed
+                if(gamepad1.left_stick_x > 0 && gamepad1.left_stick_x < 1){
+                    rightMotorPower = leftMotorPower/2;
+                }
+                
+                else if(gamepad1.left_stick_x < 0 && gamepad1.left_stick_x > -1){
+                    leftMotorPower = rightMotorPower/2;
+                }
+            }
+            
+            //left_trigger
+            else if(gamepad1.left_trigger > 0){
+                
+                //Defining te the powers of both mototrs according the left_trigger positions
+                rightMotorPower = gamepad1.left_trigger;
+                leftMotorPower = gamepad1.left_trigger;
+                
+                //Setting the range of the motor power
+                rightMotorPower = Range.clip(rightMotorPower, 0.0, -1.0);
+                leftMotorPower = Range.clip(leftMotorPower, 0.0, -1.0);
+            }
+            
+            //Setting mototrPower to 0 if none trigger is in action
+            else {
+                rightMotorPower = 0;
+                leftMotorPower = 0;
+            }
+            
+            //Sending the powers for wheel to the motors
+            robot.leftWheel.setPower(leftMotorPower);
+            robot.rightWheel.setPower(rightMotorPower);
+        }
+    }
+}
