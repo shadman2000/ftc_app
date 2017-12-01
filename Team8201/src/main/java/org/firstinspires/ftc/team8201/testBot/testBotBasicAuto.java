1509b5ca30aaa4cc19145de70b60dc34126b2b48
Shadman Chowdhury <shadman.c19@bronxsoftware.org>
2017-12-01 20:49:19 +0000

    Autonomus for testBot

--- Team8201/src/main/java/org/firstinspires/ftc/team8201/testBot/testBotBasicAuto.java
+++ Team8201/src/main/java/org/firstinspires/ftc/team8201/testBot/testBotBasicAuto.java
@@ -12,6 +12,90 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 public class testBotBasicAuto extends LinearOpMode {
 
     HardwarePushbot robot   = new HardwarePushbot();    // Use a Pushbot's hardware
-    private ElapsedTime     runtime = new ElapsedTime();
+    private ElapsedTime runtime = new ElapsedTime();    //runtime instance
+    
+    //Declaring the variables
+    double drive = 0.5;
+    double turn = 0.4;
+    
+    @Override
+    public void runOpMode() {
+        
+        //Initializing the Hardwarek9bot
+        robot.init(hardwareMap);
+        
+        //Ready message
+        telemetry.addData("Say","Autonomous is ready");
+        telemetry.update();
+        
+        //Wait for start
+        waitForStart();
+        
+        //Let's get something for OpMode
+        //Go forward for 3 second
+        //Turn left 2 second
+        //Go forward 2 second
+        //go back 2 second
+        //turn left 2 second
+        //Go forward 3 second
+        //Displacement should be 0 (For Physics MSDUKE <3)
+        
+        //Step 1
+        robot.leftWheel.setPosition(drive);
+        robot.rightWheel.setPosition(drive);
+        runtime.reset();
+        while(opModeIsActive() && (runtime.seconds() < 3.0)){
+            telemetry.addData("Path","Leg 1: %2.5f S Elapsed",runtime.seconds());
+            telemetry.update();
+        }
+        
+        //Step 2
+        robot.leftWheel.setPosition(-drive);
+        robot.rightWheel.setPosition(drive);
+        runtime.reset();
+        while(opModeIsActive() && (runtime.seconds() < 1.5)){
+            telemetry.addData("Path","Leg 2: %2.5f S Elapsed",runtime.seconds());
+            telemetry.update();
+        }
+        
+        //Step 3
+        robot.leftWheel.setPosition(drive);
+        robot.rightWheel.setPosition(drive);
+        runtime.reset();
+        while(opModeIsActive() && (runtime.seconds() < 2.0)){
+            telemetry.addData("Path","Leg 2: %2.5f S Elapsed",runtime.seconds());
+            telemetry.update();
+        }
+        
+        //Step 4
+        robot.leftWheel.setPosition(-drive);
+        robot.rightWheel.setPosition(-drive);
+        runtime.reset();
+        while(opModeIsActive() && (runtime.seconds() < 2.0)){
+            telemetry.addData("Path","Leg 2: %2.5f S Elapsed",runtime.seconds());
+            telemetry.update();
+        }
+        
+        //Step 5
+        robot.leftWheel.setPosition(-drive);
+        robot.rightWheel.setPosition(drive);
+        runtime.reset();
+        while(opModeIsActive() && (runtime.seconds() < 1.5)){
+            telemetry.addData("Path","Leg 2: %2.5f S Elapsed",runtime.seconds());
+            telemetry.update();
+        }
+        
+        //Step 6
+        robot.leftWheel.setPosition(drive);
+        robot.rightWheel.setPosition(drive);
+        runtime.reset();
+        while(opModeIsActive() && (runtime.seconds() < 3.0)){
+            telemetry.addData("Path","Leg 2: %2.5f S Elapsed",runtime.seconds());
+            telemetry.update();
+        }
+        
+        
+        
+    }
 
 }
