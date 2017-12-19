9fc969b8cfaee69ee01166333fb8aee93ad98119
Shadman Chowdhury <shadman.c19@bronxsoftware.org>
2017-12-19 16:20:02 +0000

    First of Autonomus

---
+++ Team8201/src/main/java/org/firstinspires/ftc/team8201/autonomusBlue.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.team8201;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Blue", group = "Autonomous")
+public class Auto_PlaceCube_Blue extends LinearOpMode {
+    HardwareK9bot robot = new HardwareK9bot();
+    private ElapsedTime runtime = new ElapsedTime();
+    
+    @Override
+    public void runOpMode() {}
+}
