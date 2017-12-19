25f66c9f0782de58959b29d8489a466521c8a920
Student <student@3-34.78x660.nycboe.org>
2017-12-19 11:25:23 -0500

    SO MUCH ISSUES

--- Team8201/src/main/java/org/firstinspires/ftc/team8201/autonomusBlue.java
+++ Team8201/src/main/java/org/firstinspires/ftc/team8201/autonomusBlue.java
@@ -2,14 +2,17 @@ package org.firstinspires.ftc.team8201;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Blue", group = "Autonomous")
-public class Auto_PlaceCube_Blue extends LinearOpMode {
+public class autonomusBlue extends LinearOpMode {
     HardwareK9bot robot = new HardwareK9bot();
     private ElapsedTime runtime = new ElapsedTime();
-    
+
+    //Start declaring the variables
+
     @Override
-    public void runOpMode() {}
+    public void runOpMode() {
+
+    }
 }
