dcc3bd77c2be06efd9bbf3aef5c62cfcff7d3bdd
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2018-01-11 20:24:46 -0800

    vuforia

---
+++ Team8201/src/main/java/org/firstinspires/ftc/team8201/vuforiaTest.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.team8201;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+@Autonomous(name="Concept: VuMark Id", group ="Concept")
+
+public class vuforiaTest extends LinearOpMode {
+
+    //Crateing the Object data type
+    VuforiaLocalizer vuforia;
+
+    @Override public void runOpMode() {
+
+        //Local RC camera
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()); //If u wanna see the camera on the RC
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        //License key
+        parameters.vuforiaLicenseKey = "ATsODcD/////AAAAAVw2lR...d45oGpdljdOh5LuFB9nDNfckoxb8COxKSFX";
+
+        //Use the back camera
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        //Load the relica recovery vuforia asset
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+
+        waitForStart();
+
+        relicTrackables.activate();
+
+        while (opModeIsActive()) {
+
+            //Create an instance vuMark
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+
+            if(vuMark != RelicRecoveryVuMark.UNKNOWN){
+                telemetry.addData("VuMark", "%s visible", vuMark);
+            }else {
+                telemetry.addData("VuMark", "not visible");
+            }
+            if(vuMark.equals("LEFT")){
+                telemetry.addData("testLeft","works.equals");
+            }
+            telemetry.addData("test",vuMark);
+            telemetry.update();
+        }
+    }
+}
